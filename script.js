7cd71d156fe9c2b97a16912c18d6960182dde54b
Arijitnaskar12 <117929311+Arijitnaskar12@users.noreply.github.com>
2023-05-27 17:09:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,24 +5,28 @@ let DivElement=document.getElementById("output");
 let prom1=new Promise((resolve,reject)=>{
 	setTimeout(()=>{
 		resolve("1st");
-	},1000););
+	},1000);
+});
 let prom2=new Promise((resolve,reject)=>{
 	setTimeout(()=>{
 		resolve("2nd");
-	},2000););
+	},2000);
+});
 let prom3=new Promise((resolve,reject)=>{
 	setTimeout(()=>{
 		resolve("3rd");
-	},3000););
+	},3000);
+});
 let prom4=new Promise((resolve,reject)=>{
 	setTimeout(()=>{
 		resolve("4th");
-	},4000););
+	},4000);
+});
 let prom5=new Promise((resolve,reject)=>{
 	setTimeout(()=>{
 		resolve("5th");
 	},5000);
-	);
+});
 // Do not change the code above this
 // add your promises to the array `promises`
 let proms=Promise.any([prom1,prom2,prom3,prom4,prom5]);
